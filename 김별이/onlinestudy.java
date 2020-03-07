9366d16a8a0627dcdefcc00ae0372833c972ea63
유종원 <onesummerdrive@google.com>
2020-03-07 23:36:43 +0900

    김별이: Program1_01.java

---
+++ 김별이/onlinestudy.java
@@ -0,0 +1,33 @@
+
+public class onlinestudy {
+
+	public static void main(String[] args) {
+		
+//		숫자 찍기 과제 모음  (for 문만 사용합니다. if문 사용 금지)
+
+//				1-1)
+//				 
+//				1    2    3    4    5
+//				
+//				6    7    8    9   10
+//				
+//				11   12   13   14   15
+//				
+//				16   17   18   19   20
+//				
+//				21   22   23   24   25
+		
+		int n = 5;
+		int x = 1;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.printf("%2d ", x++);
+			}
+			System.out.println("\n");
+
+		 		}
+			
+	}
+
+}
