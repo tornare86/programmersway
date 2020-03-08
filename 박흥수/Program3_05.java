328299dd92daf226bdecc8ad7366663d46690902
유종원 <onesummerdrive@google.com>
2020-03-08 21:46:58 +0900

    박흥수: Program3_06.java

--- 박흥수/Program3_05.java
+++ 박흥수/Program3_05.java
@@ -1,31 +1,29 @@
-package PDF;
+public class Program3_05 {
+   public static void main(String[] args) {
 
-public class Program3_05a {
+      int n = 7;
 
-	public static void main(String[] args) {
+      // [05]
+      System.out.print("3-5) number = 7\n");
+      System.out.println();
 
-		int n = 7;
+      int[][] num = new int[n][n];
 
-		// [05]
-		System.out.print("3-5) number = 7\n");
-		System.out.println();
+      for (int i = 0; i < n; i++)
+         for (int j = 0; j < n; j++) {
+            num[0][j] = 9;
+            num[i][0] = 9;
+            num[i][i] = 9;
+            num[i][n - 1] = 9;
+            num[n - 1][j] = 9;
+            num[i][n - 1 - i] = 9;
+         }
 
-		int[][] num = new int[n][n];
+      for (int i = 0; i < n; i++) {
+         for (int j = 0; j < n; j++)
+            System.out.printf("%2d ", num[i][j]);
+         System.out.println();
+      }
 
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++) {
-				num[0][j] = 9;
-				num[i][0] = 9;
-				num[i][i] = 9;
-				num[i][n-1] = 9;
-				num[n-1][j] = 9;
-			}
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++)
-				System.out.printf("%2d ", num[i][j]);
-			System.out.println();
-		}
-
-	}
+   }
 }
