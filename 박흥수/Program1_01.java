92cd4fc42c9f29128315fb7b58b8c5c604508d87
유종원 <onesummerdrive@google.com>
2020-03-08 17:36:51 +0900

    박흥수: Program2_10.java - good

--- 박흥수/Program1_01.java
+++ 박흥수/Program1_01.java
@@ -10,7 +10,7 @@ public class Program1_01 {
 		System.out.print("1-1) number = 5\n");
 		System.out.println();
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) { 
 			for (int j = 1; j <= n; j++)
 				System.out.printf("%2d ", n * i + j);
 			System.out.println();
